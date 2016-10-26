17a54d4fdffcea3f9fcb8cd8335af479fac58ea4
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-26 20:57:54 +0900

    Add request

---
+++ app/src/main/java/com/example/interactingwithotherapps/ShareActivity.java
@@ -0,0 +1,18 @@
+package com.example.interactingwithotherapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+
+    public void share(View view) {
+
+    }
+}
