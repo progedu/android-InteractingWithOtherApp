daf02761a89b1e56ae5da9092ffac3d6536242be
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-10-26 21:02:27 +0900

    Add handle intent

--- app/src/main/java/com/example/interactingwithotherapps/ShareActivity.java
+++ app/src/main/java/com/example/interactingwithotherapps/ShareActivity.java
@@ -1,8 +1,11 @@
 package com.example.interactingwithotherapps;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class ShareActivity extends AppCompatActivity {
 
@@ -10,6 +13,18 @@ public class ShareActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        // Get the intent that started this activity
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+
+        // Figure out what to do based on the intent type
+        if (intent.getType().indexOf("image/") != -1) {
+            // Handle intents with image data ..
+        } else if (intent.getType().equals("text/plain")) {
+            TextView textView = (TextView) findViewById(R.id.share_text);
+            textView.setText("Sent text: " + intent.getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 
     public void share(View view) {
